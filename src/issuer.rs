487d2203fda114683281664c7b8f4295c70ae66f
Dan Yamamoto <dan@iij.ad.jp>
2023-08-02 09:57:40 +0000

    use `rdf_canon::sort_graph`

--- src/issuer.rs
+++ src/issuer.rs
@@ -1,6 +1,6 @@
 use crate::{error::SignError, vc::VerifiableCredential};
 use oxrdf::{Graph, Term};
-use rdf_canon::{issue_graph, relabel_graph};
+use rdf_canon::{issue_graph, relabel_graph, sort_graph};
 
 pub fn sign(
     unsecured_credential: &VerifiableCredential,
@@ -14,8 +14,9 @@ pub fn sign(
 }
 
 fn transform(unsecured_document: &Graph) -> Result<Vec<Term>, SignError> {
-    let canonicalized_document =
-        relabel_graph(unsecured_document, &issue_graph(unsecured_document)?);
+    let canonicalized = relabel_graph(unsecured_document, &issue_graph(unsecured_document)?)?;
+    let canonicalized_triples = sort_graph(&canonicalized);
+
     todo!();
 }
 
@@ -36,4 +37,34 @@ fn add_proof_value(
     proof_value: String,
 ) -> VerifiableCredential {
     todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::transform;
+    use oxrdf::Graph;
+    use oxttl::NTriplesParser;
+    use std::io::Cursor;
+
+    #[test]
+    fn transform_simple() -> () {
+        let unsecured_document_ntriples = r#"
+<http://example.org/vicred/a> <https://www.w3.org/2018/credentials#expirationDate> "2023-12-31T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+<http://example.org/vicred/a> <https://www.w3.org/2018/credentials#issuanceDate> "2020-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+<http://example.org/vicred/a> <https://www.w3.org/2018/credentials#credentialSubject> <http://example.org/vaccine/a> .
+<http://example.org/vicred/a> <https://www.w3.org/2018/credentials#issuer> <did:example:issuer3> .
+<http://example.org/vicred/a> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.w3.org/2018/credentials#VerifiableCredential> .
+<http://example.org/vaccine/a> <http://schema.org/name> "AwesomeVaccine" .
+<http://example.org/vaccine/a> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://example.org/vocab/Vaccine> .
+<http://example.org/vaccine/a> <http://schema.org/status> "active" .
+<http://example.org/vaccine/a> <http://schema.org/manufacturer> <http://example.org/awesomeCompany> .
+"#;
+        let unsecured_document = Graph::from_iter(
+            NTriplesParser::new()
+                .parse_from_read(Cursor::new(unsecured_document_ntriples))
+                .into_iter()
+                .map(|x| x.unwrap()),
+        );
+        let transformed_document = transform(&unsecured_document);
+    }
 }
