5cc3a82a864eeb7674b058a7656a66debfd9e4ae
Dan Yamamoto <dan@iij.ad.jp>
2023-08-24 02:15:26 +0000

    refactor context

--- src/context.rs
+++ src/context.rs
@@ -1,12 +1,17 @@
 use oxrdf::NamedNodeRef;
 
+// http://purl.org/dc/terms/
 pub const CREATED: NamedNodeRef = NamedNodeRef::new_unchecked("http://purl.org/dc/terms/created");
+
+// https://www.w3.org/2018/credentials#
 pub const VERIFIABLE_CREDENTIAL_TYPE: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://www.w3.org/2018/credentials#VerifiableCredential");
 pub const VERIFIABLE_PRESENTATION_TYPE: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://www.w3.org/2018/credentials#VerifiablePresentation");
 pub const VERIFIABLE_CREDENTIAL: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://www.w3.org/2018/credentials#verifiableCredential");
+
+// https://w3id.org/security#
 pub const DATA_INTEGRITY_PROOF: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://w3id.org/security#DataIntegrityProof");
 pub const PROOF: NamedNodeRef = NamedNodeRef::new_unchecked("https://w3id.org/security#proof");
@@ -20,10 +25,12 @@ pub const VERIFICATION_METHOD: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://w3id.org/security#verificationMethod");
 pub const ASSERTION_METHOD: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://w3id.org/security#assertionMethod");
-pub const FILTER: NamedNodeRef = NamedNodeRef::new_unchecked("https://zkp-ld.org/security#filter");
 pub const PUBLIC_KEY_MULTIBASE: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://w3id.org/security#publicKeyMultibase");
 pub const SECRET_KEY_MULTIBASE: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://w3id.org/security#secretKeyMultibase");
 pub const MULTIBASE: NamedNodeRef =
     NamedNodeRef::new_unchecked("https://w3id.org/security#multibase");
+
+// https://zkp-ld.org/security#
+pub const FILTER: NamedNodeRef = NamedNodeRef::new_unchecked("https://zkp-ld.org/security#filter");
