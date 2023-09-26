75f32552028c7b8993544fb20a41e8125c04e9ba
Dan Yamamoto <dan@iij.ad.jp>
2023-09-26 13:10:58 +0000

    update blind sign interfaces: * rename `blind_sign_request` to `request_blind_sign` * rename `verify_blind_sig_request` to `verify_blind_sign_request` * add `blind_sign_request` to the optional parameter of `derive_proof` instead of boolean `commit_secret` * make generation of PoK in `request_blind_sign` optional (for the case of using the blind sign request with `derive_proof`)

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,9 @@ mod vc;
 mod verify_proof;
 
 pub use blind_signature::{
-    blind_sign, blind_sign_request, blind_sign_request_string, blind_sign_string, blind_verify,
-    blind_verify_string, unblind, unblind_string, verify_blind_sig_request,
-    verify_blind_sig_request_string,
+    blind_sign, request_blind_sign, request_blind_sign_string, blind_sign_string, blind_verify,
+    blind_verify_string, unblind, unblind_string, verify_blind_sign_request,
+    verify_blind_sign_request_string, BlindSignRequest, BlindSignRequestString,
 };
 pub use common::{ark_to_base64url, ark_to_multibase, multibase_to_ark};
 pub use derive_proof::{derive_proof, derive_proof_string};
